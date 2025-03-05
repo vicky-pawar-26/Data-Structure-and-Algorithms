ca8b2fd5d195f8f9707d6372e79dc15a732793ae
Vicky Pawar <68545298+vicky-pawar-26@users.noreply.github.com>
2025-03-05 13:06:22 +0530

    Update KadaneAlgorithm.cpp

--- Array/KadaneAlgorithm.cpp
+++ Array/KadaneAlgorithm.cpp
@@ -1,6 +1,10 @@
+// leetcode 53 maximum subarray
 class Solution
 {
 public:
+    // Approch no. 1 - find out all subarray and sum 
+    // TC: o(n^2)
+    // SC: o(1)
     int naive(vector<int> &nums)
     {
         int ans = INT_MIN;
@@ -9,17 +13,17 @@ public:
             int sum = 0;
             for (int j = i; j < nums.size(); j++)
             {
-                sum = sum + nums[j]; // saare subarrays ka
-                                     // sum nikalega, with nums[i] as pivot.
+                sum = sum + nums[j]; 
+                                    
                 ans = max(ans, sum);
-                // if(sum > ans)
-                //     ans = sum;
             }
         }
         return ans;
     }
 
-    // famous algo // TC: O(n)
+    // Approcch no. 2 - usnig kedane algo
+    // TC: o(n^2)
+    // SC: o(1)
     int kadaneAlgo(vector<int> &nums)
     {
         int ans = INT_MIN, sum = 0;
