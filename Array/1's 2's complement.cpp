027184077f90ee511e21d9ae39b832e7cbb345e1
Vicky Pawar <68545298+vicky-pawar-26@users.noreply.github.com>
2025-03-05 12:29:05 +0530

    Update 1's 2's complement.cpp

--- Array/1's 2's complement.cpp
+++ Array/1's 2's complement.cpp
@@ -1,47 +1,50 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
-
 using namespace std;
 
-vector<int> find2sComplement(vector<int> binary)
-{
-    int n = binary.size();
-    vector<int> twosComp(n, 0);
-
-    // step 1: flip
-    for (int i = 0; i < n; i++)
-    {
-        if (binary[i] == 0)
-            twosComp[i] = 1;
-        else if (binary[i] == 1)
-            twosComp[i] = 0;
-    }
-
-    // step 2: add 1
-    int carry = 1;
-    for (int i = twosComp.size() - 1; i >= 0; i--)
-    {
-        int sum = twosComp[i] + carry;
-        twosComp[i] = sum % 2;
-        carry = sum / 2;
-
-        if (carry == 0)
-        {
-            break;
-        }
-    }
-    return twosComp;
+void print(vector<int> binary){
+  for(int i = 0; i < binary.size(); i++){
+    cout<<binary[i]<<" ";
+  }
+  cout<<endl;
 }
 
-int main()
-{
-    vector<int> binary = {0, 1, 1, 0, 1};
-    vector<int> twosComp = find2sComplement(binary);
-    for (int i = 0; i < twosComp.size(); i++)
-    {
-        cout << twosComp[i] << " ";
+vector<int> find2sComplement(vector<int> binary){
+  int n = binary.size();
+  vector<int> ans(n+1, 0);
+
+  // step: 1 - flip all the numbers
+  int index = ans.size() - 1;
+  for(int i = n - 1; i >= 0; i--){
+    if(binary[i] == 0){
+      ans[index] = 1;
     }
-    cout << endl;
-    return 0;
+    else{
+      ans[index] = 0;
+    }
+    index--;
+  }
+  
+  // step: 2- add one
+  int carry = 1;
+  for(int i = ans.size() - 1; i >= 0; i--){
+    int sum = ans[i] + carry;
+    ans[i] = sum % 2;
+    carry = sum / 2;
+  }
+
+  if(carry){
+    ans[0] = carry;
+  }
+
+  return ans;
+}
+
+int main() {
+  
+  vector<int> binary = {0, 1, 1, 0, 1};
+  vector<int> twosComp = find2sComplement(binary);
+  print(twosComp);
+  cout << endl;
+  return 0;
 }
